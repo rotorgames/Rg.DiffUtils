47ce5eec54c620f34ef43a6b7381990159a0eac8
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2021-06-19 14:41:56 +0400

    feat(DiffUtil): Implement all features

---
+++ Rg.DiffUtils/DiffOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Rg.DiffUtils
+{
+    public class DiffOptions
+    {
+        public bool AllowBatching { get; set; } = true;
+
+        public bool DetectMoves { get; set; } = true;
+    }
+}
